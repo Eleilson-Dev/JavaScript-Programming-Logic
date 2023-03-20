bde8b01703945904fbf28165fdec9ac464d404b4
Elleylson Santtos <maxta.gamer@gmail.com>
2023-03-20 00:19:39 -0300

    adicionei o prettier e o eslint no projeto

--- exs-capitulo-04/ex-02/script.jsx
+++ exs-capitulo-04/ex-02/script.jsx
@@ -2,21 +2,19 @@ const numero = document.querySelector('#numero');
 const res = document.querySelector('#res');
 const btn = document.querySelector('#btn');
 let numbers = '';
-
 const tabuada = () => {
   const num = Number(numero.value);
   for (let i = num; i > 0; i--) {
     numbers += `${i} <br>`;
   }
   res.innerHTML = `Entre ${num} e 1: <br> ${numbers}`;
-}
-
+};
 btn.addEventListener('click', () => {
-  if(numero.value === '' || isNaN(numero.value)) {
+  if (numero.value === '' || isNaN(numero.value)) {
     alert('Adicione um valor valido');
-    return
+    return;
   }
   res.innerHTML = '';
-  numbers = ''
-  tabuada()
-})
+  numbers = '';
+  tabuada();
+});
